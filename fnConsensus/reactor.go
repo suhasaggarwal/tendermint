b0ec9c632d5b87f8f005733a44d7b470ef760e78
Parth <desaiparth08@gmail.com>
2019-02-13 22:03:33 +0530

    update reactor with latest type

--- fnConsensus/reactor.go
+++ fnConsensus/reactor.go
@@ -21,14 +21,17 @@ type FnConsensusReactor struct {
 	db             dbm.DB
 	tmStateDB      dbm.DB
 	chainID        string
+
+	fnRegistry *FnRegistry
 }
 
-func NewFnConsensusReactor(chainID string, db dbm.DB, tmStateDB dbm.DB) *FnConsensusReactor {
+func NewFnConsensusReactor(chainID string, fnRegistry *FnRegistry, db dbm.DB, tmStateDB dbm.DB) *FnConsensusReactor {
 	reactor := &FnConsensusReactor{
 		connectedPeers: make(map[p2p.ID]p2p.Peer),
 		db:             db,
 		chainID:        chainID,
 		tmStateDB:      tmStateDB,
+		fnRegistry:     fnRegistry,
 	}
 
 	reactor.BaseReactor = *p2p.NewBaseReactor("FnConsensusReactor", reactor)
@@ -97,22 +100,22 @@ func (f *FnConsensusReactor) Receive(chID byte, peer p2p.Peer, msgBytes []byte) 
 			f.Logger.Error("FnConsensusReactor: Invalid Data passed, ignoring...")
 			return
 		}
-		meta := remoteVoteSet.Meta(f.chainID, currentState.Validators)
-		if !meta.IsValid {
+
+		if !remoteVoteSet.IsValid(f.chainID, currentState.Validators, f.fnRegistry) {
 			f.Logger.Error("FnConsensusReactor: Invalid VoteSet specified, ignoring...")
 			return
 		}
 
-		if meta.IsMaj23 {
+		if remoteVoteSet.IsMaj23(currentState.Validators) {
 			f.Logger.Error("FnConsensusReactor: Protocol violation: Received VoteSet with majority of validators signed, Ignoring...")
 			return
 		}
 
 		// TODO: Check nonce with mainnet before accepting remote vote set
-		if f.state.CurrentVoteSet == nil {
-			f.state.CurrentVoteSet = remoteVoteSet
+		if f.state.CurrentVoteSets[remoteVoteSet.GetFnID()] == nil {
+			f.state.CurrentVoteSets[remoteVoteSet.GetFnID()] = remoteVoteSet
 		} else {
-			if err := f.state.CurrentVoteSet.Merge(remoteVoteSet); err != nil {
+			if err := f.state.CurrentVoteSets[remoteVoteSet.Payload.Request.FnID].Merge(remoteVoteSet); err != nil {
 				f.Logger.Error("FnConsensusReactor: Unable to merge remote vote set into our own.", "error:", err)
 				return
 			}
@@ -120,7 +123,9 @@ func (f *FnConsensusReactor) Receive(chID byte, peer p2p.Peer, msgBytes []byte) 
 
 		if f.areWeValidator() {
 			// Add our vote
-			// If it becomes maj23, call Oracle and return
+			if f.state.CurrentVoteSets[remoteVoteSet.GetFnID()].IsMaj23(currentState.Validators) {
+				// Inform oracle and return
+			}
 		}
 
 		// Propogate voteset to all nodes if voteSet is not identical to the one sent by Peer, otherwise send to all nodes minus peer who sent it
