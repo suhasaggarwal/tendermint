889172644cbe581e073b3055a133c09f250096c7
Parth <desaiparth08@gmail.com>
2019-02-14 19:13:43 +0530

    add function to save Reactor state

--- fnConsensus/store.go
+++ fnConsensus/store.go
@@ -16,3 +16,18 @@ func LoadReactorState(db dbm.DB) (*ReactorState, error) {
 	}
 	return persistedRectorState, nil
 }
+
+func SaveReactorState(db dbm.DB, reactorState *ReactorState, sync bool) error {
+	marshalledBytes, err := reactorState.Marshal()
+	if err != nil {
+		return err
+	}
+
+	if sync {
+		db.SetSync([]byte(ReactorStateKey), marshalledBytes)
+	} else {
+		db.Set([]byte(ReactorStateKey), marshalledBytes)
+	}
+
+	return nil
+}
